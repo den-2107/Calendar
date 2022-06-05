b6084d483782cccc03f2afa8720d76f1d3dfd5cd
den-2107 <stalkerden69@gmail.com>
2022-06-05 20:59:20 +0300

    create app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import moment from "moment";
+
 function App() {
+
+  
+
+  window.moment=moment;
+
+  moment.updateLocale('ru', {week: {dow:1}})
+  const startDay = moment().startOf('month').startOf('week');
+  const endDay = moment().endOf('month').endOf('week');
+
+  // console.log(startDay.format ("YYYY-MM-DD"));
+  // console.log(endDay.format ("YYYY-MM-DD"));
+
+  const calendar = [];
+  const day = startDay.clone();
+
+  while (!day.isAfter(endDay)) {
+    calendar.push(day.clone());
+    day.add( 1, 'day');
+  }
+
+  console.log(calendar);
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
